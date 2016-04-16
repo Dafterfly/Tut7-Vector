8413773d9e9a50cebd817e4ab6a819535522f033
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-04-16 23:37:45 +0200

    Generate a vector of 20 random integers

--- Vect/Vector.cpp
+++ Vect/Vector.cpp
@@ -9,4 +9,13 @@ int main()
 {
 	vector <int> vect; // create an arbitrary sized vector
 
+	int randomNo;
+	// Generate vector
+	srand(time(NULL)); // seed randomizer
+	for (i = 0; i < 20; i++)
+	{
+		randomNo = rand() % 99 + 1; // randomize numbers across an arbitrary range
+		vect.push_back(randomNo);
+	}
+
 }
